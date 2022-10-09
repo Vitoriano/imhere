c91fcdb4e068d19256f9dc445b831266341d51d0
Vitoriano <vitorianoernandes2012@gmail.com>
2022-10-08 21:30:18 -0300

    Fisrt commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fbfbfb',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <Text>Olá Mundo!</Text>
+  )
+}
